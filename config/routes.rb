7efcff531de0b810dd91df7bdeb64f21f12107f0
madcido <fabowow@gmail.com>
2019-05-06 20:11:14 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
