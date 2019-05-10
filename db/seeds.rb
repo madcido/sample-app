bd9fa98e7b7306f551e74b47bc1402a6e8f1cc50
madcido <fabowow@gmail.com>
2019-05-10 18:50:47 -0300

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
-    password_confirmation: "foobar",
+    password_confirmation: "foobar")
 
 User.create!(name:  "Fabio",
     email: "fabowow@gmail.com",
